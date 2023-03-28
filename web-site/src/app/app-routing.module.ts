5fe7d4cd74131ef08af5b4195075047935f9d84c
Nichifor Dragos <dragos.nichifor@stud.ubbcluj.ro>
2023-03-28 13:10:18 +0300

    update

--- web-site/src/app/app-routing.module.ts
+++ web-site/src/app/app-routing.module.ts
@@ -1,7 +1,42 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes, PreloadAllModules } from "@angular/router";
 
-const routes: Routes = [];
+import { RaceComponent } from "./components/race/race.component";
+import { PilotComponent } from "./components/pilot/pilot.component";
+import { CarComponent } from "./components/car/car.component";
+
+const routes: Routes = [
+{
+  path: '',
+        children: [
+            {
+                path: 'race-component',
+                component: RaceComponent,
+                data: {
+                    title: "Check out our races",
+                    breadcrumb: "Check out our races"
+                }
+            },
+      {
+          path: 'pilot-component',
+          component: PilotComponent,
+        data: {
+            title: "Check out our pilots",
+            breadcrumb: "Check out our pilots"
+        }
+      },
+      {
+          path: 'car-component',
+          component: CarComponent,
+        data: { 
+            title: "Check out our pilots",
+            breadcrumb: "Check out our cars"
+        }
+      }
+    ]
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- web-site/src/app/app.module.ts
+++ web-site/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarComponent } from './components/car/car.component';
+import { PilotComponent } from './components/pilot/pilot.component';
+import { RaceComponent } from './components/race/race.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarComponent,
+    PilotComponent,
+    RaceComponent,
   ],
   imports: [
     BrowserModule,
