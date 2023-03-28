39131a1fb24bdbc13f06a0c7e703e392d7ca8c5a
Nichifor Dragos <dragos.nichifor@stud.ubbcluj.ro>
2023-03-28 23:07:51 +0300

    update

--- web-site/src/app/components/car/car.component.ts
+++ web-site/src/app/components/car/car.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+import { Car } from 'src/app/core/model/car.model';
+import { CarService } from 'src/app/core/service/car.service';
 
 @Component({
   selector: 'app-car',
@@ -7,4 +12,21 @@ import { Component } from '@angular/core';
 })
 export class CarComponent {
 
+  subscriptions: Subscription[] = [];
+  cars: Car[] = []
+
+  constructor(
+    private carService: CarService,
+    private router: Router
+    ) {}
+
+    ngOnInit() {
+      this.listCars();
+    }
+
+    listCars() {
+      this.subscriptions.push(this.carService.listRaces().subscribe(cars => {
+        this.cars = cars
+      }, error => console.log("Something went wrong " + error)))
+    }
 }
--- web-site/src/app/components/pilot/pilot.component.ts
+++ web-site/src/app/components/pilot/pilot.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { PilotService } from 'src/app/core/service/pilot.service';
+
+import { Subscription } from 'rxjs';
+import { Pilot } from 'src/app/core/model/pilot.model';
 
 @Component({
   selector: 'app-pilot',
@@ -6,5 +11,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./pilot.component.css']
 })
 export class PilotComponent {
+
+  subscriptions: Subscription[] = [];
+  pilots: Pilot[] = [];
+
+  public constructor (
+    private pilotService: PilotService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.listPilots();
+  }
+
+  listPilots() : void {
+    this.subscriptions.push(this.pilotService.listRaces().subscribe(pilots => {
+      this.pilots = pilots;
+    }, error => console.log("Something went wrong " + error)))
+  }
 
 }
